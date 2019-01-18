f43761f7d0e6090b1d0ed882b40a75061fa55ffa
Daniel Lundemo <daniel.lundemo@gmail.com>
2019-01-18 05:53:39 +0100

    FIXED EEVERYTHIGN

--- static/login.js
+++ static/login.js
@@ -2,12 +2,18 @@ var socket = io();
 
 $('form').submit(function (e) {
   e.preventDefault();
-  var message = $(e.target).find('input').val();
+  var username = $(e.target).find('input[name="username"]').val();
+  var password = $(e.target).find('input[name="password"]').val();
   socket.emit('message', {
-    message: message
+    username: username,
+    password: password
   });
 });
 
 socket.on('message', function (data) {
   $('.info').append('<b>' + data.message + '<br>');
+});
+
+jQuery("input[name='mycheckbox']").each(function() {
+  console.log( this.value + ":" + this.checked );
 });
