e7b4bc36ddf2f7662c39fb5c75c961fd5c39afcc
Daniel Lundemo <daniel.lundemo@gmail.com>
2019-01-18 02:44:58 +0100

    added bold tag instead of strong

--- static/javascript.js
+++ static/javascript.js
@@ -26,7 +26,7 @@ socket.on('message', function (data) {
  if ($(".chat").html().length > 5) {
     $('.chat').slice(-1,1)
  }
-  $('.chat').append('<strong>' + data.user + '</strong>: ' + data.message + '<br>');
+  $('.chat').append('<b>' + data.user + '</b>: ' + data.message + '<br>');
 });
 
 // When the form is submitted
