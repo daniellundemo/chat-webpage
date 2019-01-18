1e2695d7ec3d6387f3372e26055cb02d0bbcebd4
Daniel Lundemo <daniel.lundemo@gmail.com>
2019-01-18 04:36:43 +0100

    added simple Database

---
+++ static/Db.py
@@ -0,0 +1,13 @@
+class Db:
+    def __init__(self):
+        self.users = {}
+
+    def add_user(self, username, password):
+        self.users[username] = password
+
+    def del_user(self, username):
+        if username in self.users:
+            del self.users[username]
+
+    def list_users(self):
+        return self.users.keys()
