0d86b443ff274c08a435a0b8f8a79f7990b02dcc
Daniel Lundemo <daniel.lundemo@gmail.com>
2019-01-20 02:49:03 +0100

    dobblefnutts

--- static/javascript.js
+++ static/javascript.js
@@ -12,7 +12,7 @@ socket.on('message', function (data) {
 socket.on('user-list', function (data) {
   $('.users').empty();
   data.users.forEach(function(element) {
-    $('.users').append('<button onclick='joinChat(element)'>' + '<b>' + element + '</b></br></button>');
+    $('.users').append('<button onclick="joinChat()">' + '<b>' + element + '</b></br></button>');
   });
 });
 
